1d98b57eb0f241fba369314333cc4b0313f5e4fb
campagnucci <134701252+thegnucci@users.noreply.github.com>
2025-08-06 11:45:15 -0300

    Nova pagina Feedback

--- web/Navigator/RootNavigator.jsx
+++ web/Navigator/RootNavigator.jsx
@@ -1,11 +1,13 @@
 // App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from '../Screens/Home/Home.jsx';
 import PaginaLogin from '../Screens/PaginaLogin/PaginaLogin.jsx';
 import AboutUs from '../Screens/AboutUs/AboutUs.jsx';
 import PageCadastro from '../Screens/PageCadastro/PageCadastro.jsx';
 import InfoPage from '../Screens/InfoPage/InfoPage.jsx';
+import Feedback from '../Screens/Feedback/Feedback.jsx';
 import ScrollToTop from '@/components/ScrollToTop/ScrollToTop';
+
 
 
 function RootNavigator() {
@@ -19,6 +21,7 @@ function RootNavigator() {
         <Route  path='/aboutUs' element={<AboutUs/>} />
         <Route  path='/pageCadastro' element={<PageCadastro/>} />
         <Route  path='/InfoPage' element={<InfoPage/>} />
+        <Route  path='/Feedback' element={<Feedback/>} />
         
       </Routes>
     </BrowserRouter>
